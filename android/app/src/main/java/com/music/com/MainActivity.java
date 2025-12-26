61b3614aa7b8c58941e74bd1d9e5b4a6bd96bdc4
Trendyzima <momanyitrendy@gmail.com>
2025-12-26 15:38:06 +0000

    Android platform ready (Capacitor sync successful)

---
+++ android/app/src/main/java/com/music/com/MainActivity.java
@@ -0,0 +1,5 @@
+package com.music.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.music.com',
+  appName: 'npm run buildnpx cap add android',
+  webDir: 'dist'
+};
+
+export default config;
